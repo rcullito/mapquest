3c2ed541cbeca8de9e058ae5b50a8bf09be1ef83
rcullito <rculliton@guaranteedrate.com>
2021-03-26 11:18:55 -0400

    upate url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mapquest "0.1.0"
   :description " tiny clojure library of functions and macros for dealing with maps"
-  :url "https://cljdoc.org/d/rcullito/mapquest/CURRENT"
+  :url "https://cljdoc.org/d/mapquest/mapquest/CURRENT"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
